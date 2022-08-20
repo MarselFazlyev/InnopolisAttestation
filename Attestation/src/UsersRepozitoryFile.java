1b42d2ffa1aecf20e000b8e92fa2188ce01c85de
Fazlyev Marsel <fazlyev@mail.ru>
2022-08-20 16:31:18 +0300

    Attestation project added

---
+++ Attestation/src/UsersRepozitoryFile.java
@@ -0,0 +1,6 @@
+public interface UsersRepozitoryFile {
+    User findById(int id);
+    void create(User user);
+    void update(User user);
+    void delete(int id);
+}
---
+++ Attestation/src/UsersRepozitoryFileImpl.java
@@ -0,0 +1,21 @@
+public class UsersRepozitoryFileImpl implements UsersRepozitoryFile{
+    @Override
+    public User findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void create(User user) {
+
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
