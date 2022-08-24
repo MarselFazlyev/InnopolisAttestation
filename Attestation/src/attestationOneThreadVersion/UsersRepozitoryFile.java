0a6b20a47254f3d14e32499ac442a5c46f5e33f2
Fazlyev Marsel <fazlyev@mail.ru>
2022-08-24 23:01:33 +0300

    directory Attestation renamed to Attestation_1

--- Attestation/src/attestationOneThreadVersion/UsersRepozitoryFile.java
+++
@@ -1,8 +0,0 @@
-package attestationThreadsVersion;
-
-public interface UsersRepozitoryFile {
-    User findById(int id);
-    void create(User user);
-    void update(User user);
-    void delete(int id);
-}
